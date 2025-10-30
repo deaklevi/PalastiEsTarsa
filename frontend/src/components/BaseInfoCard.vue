3c04acc7c66df0f85ff0006c704983658da5cedb
deaklevi <deakleventevok@gmail.com>
2025-10-30 20:15:40 +0100

    BaseInfoCard DONE

--- frontend/src/components/BaseInfoCard.vue
+++ frontend/src/components/BaseInfoCard.vue
@@ -1,10 +1,8 @@
 <template>
-    <div>
-        <img :src="image" :alt="image">
-        <p>
-            {{ text }}
-        </p>
-    </div>
+  <div class="flex flex-col items-center bg-neutral-800 border-2 border-orange-600 p-5 md:p-2 md:flex-row md:items-start">
+    <img :src="image" :alt="image" class="h-40 mb-5 md:mb-0">
+    <p class="md:ml-5 text-sm text-white text-left max-w-[500px] md:max-w-max" v-html="text"></p>
+  </div>
 </template>
 
 <script>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,13 +1,28 @@
 <template>
   <BaseLayout>
     <BaseSlider class="min-[770px]:mt-52"/>
+    <div class="flex flex-col items-center xl:items-start min-[770px]:mt-52 mt-24 xl:flex-row xl:justify-between lg:mx-12">
+      <div class="max-w-[600px] mb-24  mx-5">
+        <h1 class="mb-5 text-2xl font-bold">--Palásti és Társa kft.--</h1>
+        <p class="ml-5">
+          A <span class="text-orange-600 font-semibold">Kő</span> – legyen az gránit, márvány vagy mészkő – az életünk! A <span class="text-orange-600 font-semibold">kőfeldolgozás</span> valamennyi területén megálljuk a helyünket. 1954 óta rengeteg tapasztalatot gyűjtöttünk az egyedi kézműves síremlékek, kripták, emlékművek, szökőkutak és minden egyéb épülethez tartozó kőburkolat, kőtermék készítésében, kerüljön az kültérre vagy beltérre. Számtalan munkát készítettünk köztérre és magánszemélyek otthonába. Készítettünk 390db fürdőszoba -, recepciós - és közösségi illemhelységekbe egyéb gránit pultot a Budapest Hilton szállodának. Irodaházaknak homlokzat-, padló- ill. lépcsőburkolatokat. Családi házakba konyhapultot, külső- és belső ablakpárkányt, kerítés fedkövet. 1996 és 2006 között folyamatosan exportáltunk minimal és exluzív formavilágú sírkövet Dániába, Hollandiába, Ausztriába és Németországba. <br><br> A hazai piacra szánt sírkő termékeink a szolíd árfekvésűtől az egyedi, prémium kategóriáig bezárólag, széles választékban állnak megrendelőink rendelkezésre. Ezáltal mindenki megtalálhatja az elképzelése és lehetőségéhez leginkább illő megoldásokat. Magától értetődő, hogy a hibátlan kőtermék csak a megfelelő minőségű alapanyagból készülhet. Sok évtizedes kapcsolatok fűznek bennünket hazai, európai és a világ számos országában működő kőbányákhoz. Igény szerint, a világon bányászott természetes kövek közül bármelyik beszerezhető és elképzelés szerint feldolgozzuk félkész ill. készterméknek.
+        </p>
+      </div>
+      <div class="max-w-[800px]">
+        <BaseInfoCard class="mb-5" image="../../public/Home/sirko.jpg" text="A hazai piacra szánt kézműves <span class='text-orange-600 font-semibold'>sírkő</span> termékeink a szolíd árfekvésűtől az egyedi, prémium kategóriáig bezárólag, széles választékban állnak megrendelőink rendelkezésre. Ezáltal mindenki megtalálhatja az elképzelése és lehetőségéhez leginkább illő megoldásokat. Magától értetődő, hogy a hibátlan kőtermék csak a megfelelő minőségű alapanyagból készülhet. Sok évtizedes kapcsolatok fűznek bennünket hazai, európai és a világ számos országában működő kőbányákhoz. Igény szerint, a világon bányászott természetes kövek közül bármelyik beszerezhető és elképzelés szerint feldolgozzuk félkész ill. készterméknek." />
+        <BaseInfoCard class="mb-5" image="../../public/Home/epiteszet.jpg" text="Az <span class='text-orange-600 font-semibold'>építészet</span> oldalon felsorolt témakörök magukba foglalják tevékenységünk jelentős részét. Mint konyhapult, fürdőszobapult, lépcsőburkolat, ablakpárkány, kerítés fedkő, kőkorlát azaz baluszter korlát, kandalló, padlóburkolat, kültéri burkolatok azaz terasz-, fal-, lábazat- és homlokzatburkolat, kőbútor, emlékmű, emléktábla, szökőkút és speciális kőtermékek, mint tömb mosdókagyló, tömb zuhanytálca vagy precíziós, specifikus gépek és műszerek gépasztala. Ezek jelentős részét meglévő tervek alapján készítettük el, de az is előfordult többször, hogy csak a koncepció született meg és a szakmai tapasztalatunkra támaszkodva indult a tervezés." />
+        <BaseInfoCard class="mb-5" image="../../public/Home/szolgaltatasok.jpg" text="A <span class='text-orange-600 font-semibold'>szolgáltatások</span> oldalon bemutatjuk további tevékenységi körünket, amivel maximálisan eleget tehetünk az egyre gyakoribb különleges igényeknek. 3D tervezés, ami a sírkő készítők világában egyáltalán nem jellemző. Lézergravírozás gránitba, teljesen más technológiát igényel, mint más anyagba. Gépi, műszaki és szakmai felkészültségünknek köszönhetően hozott anyagból, bérmunkával is foglalkozunk, mint kőtömb darabolása, tábla méretre vágása, szélezése, natúr felületek fényezése, rovátkolása, flammolása azaz égetése." />
+        <BaseInfoCard class="mb-5" image="../../public/Home/ko-anyagmintak.jpg" text="A <span class='text-orange-600 font-semibold'>természetes kőzetek</span> földünk szilárd anyagának kémiailag különböző, többfelé megtalálható, nagy kiterjedésű ásványtömegei, vagy jellemző összetételű ásványtársulásai. Ezek mindig konkrét természeti folyamatban, egységesen képződnek. Az ásványokon kívül gyakran nem ásványos (amorf) anyagokat is tartalmaznak; ezek mennyisége egyes (főleg vulkáni) kőzetekben a kőzet tömegének akár 90%-ánál is több lehet. A kő anyagminták oldalon megtalálhatják aktuális kínálatunkat és ezen felül, megismerhetnek számos különleges kőanyaggal." />
+        <BaseInfoCard image="../../public/Home/egyeb-informaciok.jpg" text="Egy laikus számára rengeteg megválaszolandó <span class='text-orange-600 font-semibold'>kérdés</span> merülhet fel. Természetesen nem kell mindenhez értenünk és elég lehet, ha a hozzáértő szakértelemben megbízunk. Pl: Miért gránit? Hogyan válasszunk sírkövet? Milyen az ország, megye, város vagy a falu szerinti (helyi) temetkezési kultúra és szokások? Milyen különleges szolgáltatásokat tudunk nyújtani? Miben tudunk segíteni? Mennyibe kerül a síremlék? Megrendelés menete? Az alábbi oldalon megpróbálunk minél szerteágazóan, informatívan és érthetően néhány kérdésre megadni a válaszokat. Kérem, hogy forduljon hozzánk bizalommal kérdéseivel." />
+      </div>
+    </div>
   </BaseLayout>
 </template>
 
 <script>
 import BaseLayout from '@layouts/BaseLayout.vue'
 import BaseSlider from '@components/BaseSlider.vue'
-import BaseInfoCard from '@components/BaseInfoCard.vue';
+import BaseInfoCard from '@components/BaseInfoCard.vue'
 
 export default {
   components: {
