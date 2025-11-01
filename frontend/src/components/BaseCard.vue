0944a80a0f49586ff69bca66fc5004b00e2a99f6
deaklevi <deakleventevok@gmail.com>
2025-11-01 18:36:59 +0100

    UPGRADE BaseCard

--- frontend/src/components/BaseCard.vue
+++ frontend/src/components/BaseCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <RouterLink to="#" class="w-44 md:w-52 h-full bg-neutral-800 rounded-sm border-2 border-orange-600 responsive-card">
+  <RouterLink to="#" class="w-40 md:w-52 h-full bg-neutral-800 rounded-sm border-2 border-orange-600">
     <img :src="image" :alt="image" class="w-full h-full object-cover rounded-tl-sm rounded-tr-sm">
     <h4 class="text-lg font-semibold text-center text-white p-2" v-html="title"></h4>
   </RouterLink>
@@ -14,11 +14,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-@media (max-width: 412px) {
-  .responsive-card {
-    width: 100%;
-  }
-}
-</style>
