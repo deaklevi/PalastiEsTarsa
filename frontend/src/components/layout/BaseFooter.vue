43fb24788a10e4691efd9a296e046fd7e33eb0a2
deaklevi <deakleventevok@gmail.com>
2025-11-01 16:37:53 +0100

    BaseFooter upgrade

--- frontend/src/components/layout/BaseFooter.vue
+++ frontend/src/components/layout/BaseFooter.vue
@@ -1,26 +1,29 @@
 <template>
-    <div class="w-full flex md:flex-row flex-col justify-between p-10 bg-slate-950 text-white">
-        <div class="w-full md:text-center mb-5">
-            <h3 class="text-orange-600 font-bold text-lg mb-3">Cím:</h3>
-            <p>
-                H-1214 Budapest<br>II. Rákóczi Ferenc út 251.
-            </p>
+    <div class="w-full bg-slate-950 text-white">
+        <div class="w-full flex md:flex-row flex-col justify-between p-5 md:p-10">
+            <div class="w-full md:text-center mb-5">
+                <h3 class="text-orange-600 font-bold text-lg mb-3">Cím:</h3>
+                <p>
+                    H-1214 Budapest<br>II. Rákóczi Ferenc út 251.
+                </p>
+            </div>
+            <div class="w-full md:text-center border-y-2 md:border-y-0 md:border-x-2 border-orange-600 md:mb-0  mb-5 md:py-0 py-5">
+                <h3 class="text-orange-600 font-bold text-lg mb-3">Elérhetőség:</h3>
+                <p>
+                    Iroda: <a href="tel:+3612768246" class="text-orange-500 underline">+36-1-276-8246</a><br>Palásti István (Igazgató): <a href="tel:+36302790261" class="text-orange-500 underline">+36-30-279-0261</a><br>Deák László (Telephely vezető): <a href="tel:+36309004800" class="text-orange-500 underline">+36-30-900-4800</a><br>palasti.kft@gmail.com
+                </p>
+            </div>
+            <div class="w-full md:text-center">
+                <h3 class="text-orange-600 font-bold text-lg mb-3">Nyitvatartás:</h3>
+                <p>
+                    Hétfő - péntek: 07.30 - 16.30<br>Szombat: 09.00 - 12.00<br>Vasárnap: Zárva
+                </p>
+            </div>
         </div>
-        <div class="w-full md:text-center border-y-2 md:border-y-0 md:border-x-2 border-orange-600 md:mb-0  mb-5 md:py-0 py-5">
-            <h3 class="text-orange-600 font-bold text-lg mb-3">Elérhetőség:</h3>
-            <p>
-                Iroda: +36-1-276-8246<br>Mobil1: +36-30-279-0261<br>Mobil2: +36-30-900-4800<br>palasti.kft@gmail.com
-            </p>
-        </div>
-        <div class="w-full md:text-center">
-            <h3 class="text-orange-600 font-bold text-lg mb-3">Nyitvatartás:</h3>
-            <p>
-                Hétfő - péntek: 07.30 - 16.30<br>Szombat: 09.00 - 12.00<br>Vasárnap: Zárva
-            </p>
-        </div>
+        <h3 class="text-orange-600 font-bold pl-5">Minden jog fenntartva © - Palásti és Társa Kft.</h3>
     </div>
 </template>
 
-<script>
+<script>4
 
 </script>
