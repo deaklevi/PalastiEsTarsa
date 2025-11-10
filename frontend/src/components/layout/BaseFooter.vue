ea5ebf998c587f64f40e0c8f050b464e61ed5ff4
deaklevi <deakleventevok@gmail.com>
2025-11-10 17:08:12 +0100

    UPGRADE BaseFooter

--- frontend/src/components/layout/BaseFooter.vue
+++ frontend/src/components/layout/BaseFooter.vue
@@ -4,13 +4,21 @@
             <div class="w-full md:text-center mb-5">
                 <h3 class="text-orange-600 font-bold text-lg mb-3">Cím:</h3>
                 <p>
-                    H-1214 Budapest<br>II. Rákóczi Ferenc út 251.
+                    <a href="https://www.google.com/maps/dir/?api=1&destination=H-1214+Budapest+II.+Rákóczi+Ferenc+út+251" target="_blank" class="underline">
+                        H-1214 Budapest<br>II. Rákóczi Ferenc út 251.
+                    </a>
                 </p>
             </div>
             <div class="w-full md:text-center border-y-2 md:border-y-0 md:border-x-2 border-orange-600 md:mb-0  mb-5 md:py-0 py-5">
                 <h3 class="text-orange-600 font-bold text-lg mb-3">Elérhetőség:</h3>
                 <p>
-                    Iroda: <a href="tel:+3612768246" class="text-orange-500 underline">+36-1-276-8246</a><br>Palásti István (Igazgató): <a href="tel:+36302790261" class="text-orange-500 underline">+36-30-279-0261</a><br>Deák László (Telephely vezető): <a href="tel:+36309004800" class="text-orange-500 underline">+36-30-900-4800</a><br>palasti.kft@gmail.com
+                    Iroda: <a href="tel:+3612768246" class="underline">+36-1-276-8246</a>
+                    <br>
+                    Palásti István (Igazgató): <a href="tel:+36302790261" class="underline">+36-30-279-0261</a>
+                    <br>
+                    Deák László (Telephely vezető): <a href="tel:+36309004800" class="underline">+36-30-900-4800</a>
+                    <br>
+                    palasti.kft@gmail.com
                 </p>
             </div>
             <div class="w-full md:text-center">
