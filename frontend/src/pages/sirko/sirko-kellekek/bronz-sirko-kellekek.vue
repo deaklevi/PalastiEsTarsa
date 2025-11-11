544676b889c828ad9c36ba364711050f4318ef48
deaklevi <deakleventevok@gmail.com>
2025-11-11 02:23:19 +0100

    UPDATE bronz-sirko-kellekek AND sirko-keszites page AND DONE all sirko pages :)

--- frontend/src/pages/sirko/sirko-kellekek/bronz-sirko-kellekek.vue
+++ frontend/src/pages/sirko/sirko-kellekek/bronz-sirko-kellekek.vue
@@ -6,17 +6,48 @@
     </div>
 
     <div class="flex flex-wrap justify-center gap-5 mt-24 md:mt-52 mb-24 md:mb-52 max-w-[1500px] mx-auto">
-        <BaseInfoCard class="cursor-pointer w-[30rem]" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-kereszt-es-feszulet.jpg" text="A katalógusban látható keresztek és feszületek kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek." onclick="window.open('../../public/PDF/feszulet.pdf')"/>
-        <BaseInfoCard class="cursor-pointer w-[30rem]" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-sullyesztett-vaza-es-mecsestarto-lampa.jpg" text="A katalógusban látható süllyesztett vázák és mécsestartó lámpák kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek. " onclick="window.open('../../public/PDF/sullyesztett-vaza-lampa.pdf')"/>
-        <BaseInfoCard class="cursor-pointer w-[30rem]" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-vaza-es-mecsestarto-lampa.jpg" text="A katalógusban látható vázák és mécsestartó lámpák kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek." onclick="window.open('../../public/PDF/vaza-lampa.pdf')"/>
-        <BaseInfoCard class="cursor-pointer w-[30rem]" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-virag-es-rozsa.jpg" text="A katalógusban látható virágok és rózsák kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek." onclick="window.open('../../public/PDF/virag.pdf')"/>
-        <BaseInfoCard class="cursor-pointer w-[30rem]" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-plakett.jpg" text="A katalógusban látható plakettek kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek." onclick="window.open('../../public/PDF/plakett.pdf')"/>
-        <BaseInfoCard class="cursor-pointer w-[30rem]" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-szobor.jpg" text="A katalógusban látható szobrok kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek." onclick="window.open('../../public/PDF/szobor.pdf')"/>
-        <BaseInfoCard class="cursor-pointer w-[30rem]" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-betuk.jpg" text="A katalógusban látható különböző betű típusok, eltérő méretekkel készülnek. Minden karakter rögzítési technikája egyforma. Fúrt lyukba ragasztó kerül és a csappal ellátott betűt a felületre ragasztjuk." onclick="window.open('../../public/PDF/betu.pdf')"/>
-        <BaseInfoCard class="cursor-pointer w-[30rem]" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-kripta-karika.jpg" text="A katalógusban látható kripta karikák kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei." onclick="window.open('../../public/PDF/kripta-karika.pdf')"/>
-        <BaseInfoCard class="cursor-pointer w-[30rem]" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-rozetta.jpg" text="A katalógusban látható rozetták kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei." onclick="window.open('../../public/PDF/rozetta.pdf')"/>
-        <BaseInfoCard class="cursor-pointer w-[30rem]" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-disz-kampo.jpg" text="A katalógusban látható dísz kampók kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei." onclick="window.open('../../public/PDF/disz-kampo.pdf')"/>
+      <div class="flex flex-col items-center md:items-start w-[30rem]">
+        <h4 class="text-base font-semibold pb-2 text-center md:text-left">Bronz kereszt és feszület</h4>
+        <BaseInfoCard class="cursor-pointer w-full" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-kereszt-es-feszulet.jpg" text="A katalógusban látható keresztek és feszületek kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek." onclick="window.open('../../public/PDF/feszulet.pdf')"/>
+      </div>
+      <div class="flex flex-col items-center md:items-start w-[30rem]">
+        <h4 class="text-base font-semibold pb-2 text-center md:text-left">Bronz süllyesztett váza és mécsestartó lámpa</h4>
+        <BaseInfoCard class="cursor-pointer w-full" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-sullyesztett-vaza-es-mecsestarto-lampa.jpg" text="A katalógusban látható süllyesztett vázák és mécsestartó lámpák kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek. " onclick="window.open('../../public/PDF/sullyesztett-vaza-lampa.pdf')"/>
+      </div>
+      <div class="flex flex-col items-center md:items-start w-[30rem]">
+        <h4 class="text-base font-semibold pb-2 text-center md:text-left">Bronz váza és mécsestartó lámpa</h4>
+        <BaseInfoCard class="cursor-pointer w-full" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-vaza-es-mecsestarto-lampa.jpg" text="A katalógusban látható vázák és mécsestartó lámpák kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek." onclick="window.open('../../public/PDF/vaza-lampa.pdf')"/>
+      </div>
+      <div class="flex flex-col items-center md:items-start w-[30rem]">
+        <h4 class="text-base font-semibold pb-2 text-center md:text-left">Bronz virág és rózsa</h4>
+        <BaseInfoCard class="cursor-pointer w-full" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-virag-es-rozsa.jpg" text="A katalógusban látható virágok és rózsák kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek." onclick="window.open('../../public/PDF/virag.pdf')"/>
+      </div>
+      <div class="flex flex-col items-center md:items-start w-[30rem]">
+        <h4 class="text-base font-semibold pb-2 text-center md:text-left">Bronz plakett</h4>
+        <BaseInfoCard class="cursor-pointer w-full" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-plakett.jpg" text="A katalógusban látható plakettek kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek." onclick="window.open('../../public/PDF/plakett.pdf')"/>
+      </div>
+      <div class="flex flex-col items-center md:items-start w-[30rem]">
+        <h4 class="text-base font-semibold pb-2 text-center md:text-left">Bronz szobor</h4>
+        <BaseInfoCard class="cursor-pointer w-full" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-szobor.jpg" text="A katalógusban látható szobrok kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei. A P jelzés annyit jelent, hogy függőleges felületre, a T jelűek pedig vízszintes felszínre a talpukra szerelhetőek." onclick="window.open('../../public/PDF/szobor.pdf')"/>
+      </div>
+      <div class="flex flex-col items-center md:items-start w-[30rem]">
+        <h4 class="text-base font-semibold pb-2 text-center md:text-left">Bronz betűk</h4>
+        <BaseInfoCard class="cursor-pointer w-full" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-betuk.jpg" text="A katalógusban látható különböző betű típusok, eltérő méretekkel készülnek. Minden karakter rögzítési technikája egyforma. Fúrt lyukba ragasztó kerül és a csappal ellátott betűt a felületre ragasztjuk." onclick="window.open('../../public/PDF/betu.pdf')"/>
+      </div>
+      <div class="flex flex-col items-center md:items-start w-[30rem]">
+        <h4 class="text-base font-semibold pb-2 text-center md:text-left">Bronz kripta karika</h4>
+        <BaseInfoCard class="cursor-pointer w-full" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-kripta-karika.jpg" text="A katalógusban látható kripta karikák kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei." onclick="window.open('../../public/PDF/kripta-karika.pdf')"/>
+      </div>
+      <div class="flex flex-col items-center md:items-start w-[30rem]">
+        <h4 class="text-base font-semibold pb-2 text-center md:text-left">Bronz rozetta</h4>
+        <BaseInfoCard class="cursor-pointer w-full" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-rozetta.jpg" text="A katalógusban látható rozetták kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei." onclick="window.open('../../public/PDF/rozetta.pdf')"/>
+      </div>
+      <div class="flex flex-col items-center md:items-start w-[30rem]">
+        <h4 class="text-base font-semibold pb-2 text-center md:text-left">Bronz díszkampós</h4>
+        <BaseInfoCard class="cursor-pointer w-full" image="../../public/Sirko/Sirko-kellekek/Bronz-sirko-kellekek/bronz-disz-kampo.jpg" text="A katalógusban látható dísz kampók kódszámmal vannak ellátva. A kódszám alatt található az adott forma különböző méretei." onclick="window.open('../../public/PDF/disz-kampo.pdf')"/>
+      </div>
     </div>
+
   </BaseLayout>
 </template>
 
