de058b5e1cbf59d64c2c565e39edef4b991e797a
deaklevi <deakleventevok@gmail.com>
2025-11-22 17:17:05 +0100

    UPGRADE ArchitecturaSeeder

--- backend/database/migrations/2025_11_11_162014_create_architectures_table.php
+++ backend/database/migrations/2025_11_11_162014_create_architectures_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('architectures', function (Blueprint $table) {
             $table->id();
             $table->integer("order");
-            $table->string("name", 50);
+            $table->string("name", 100);
             $table->string("image_url", 255)->nullable();
             $table->string("group", 50);
             $table->timestamps();
