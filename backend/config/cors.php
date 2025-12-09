25ab15fdb4084b62e5060b72487890357d2de38e
deaklevi <deakleventevok@gmail.com>
2025-12-09 02:07:06 +0100

    UPGRADE mobile view

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'send-contact'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://192.168.1.83:5173', 'http://localhost:5173'],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
