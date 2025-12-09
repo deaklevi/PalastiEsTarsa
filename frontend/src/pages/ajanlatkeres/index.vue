47a541a75ccf89b19468e464534ecff7f1632c08
deaklevi <deakleventevok@gmail.com>
2025-12-09 17:01:23 +0100

    UPGRADE

--- frontend/src/pages/ajanlatkeres/index.vue
+++ frontend/src/pages/ajanlatkeres/index.vue
@@ -5,78 +5,84 @@
       <h2 class="mt-5 md:mt-10 text-xl font-bold w-full text-center md:text-left">Ajánlatkérés</h2>
     </div>
 
-    <div class="flex flex-col items-center md:mx-12">
-      <p class="mt-5 md:mt-10 text-base mx-10 text-left"> Az alábbbi űrlap alapvetően síremlék árajánlat kérésére vontakozik. Abban az esetben, ha más, egyéb termék iránt érdeklődik, a kapcsolat oldalon lévő üzenetdobozban elküldheti ajánlatkérését vagy közvetlenül megkereshet minket e-mail-en vagy telefonon. </p>
+    <div class="mx-auto mt-5 md:mt-10 w-[90%] lg:max-w-4xl">
+      <p class="text-base text-left"> Az alábbbi űrlap alapvetően síremlék árajánlat kérésére vontakozik. Abban az esetben, ha más, egyéb termék iránt érdeklődik, a kapcsolat oldalon lévő üzenetdobozban elküldheti ajánlatkérését vagy közvetlenül megkereshet minket e-mail-en vagy telefonon. </p>
     </div>
 
     <!-- Ajánlatkérés form -->
-    <div class="bg-slate-950 md:w-full max-w-lg p-10 mx-auto rounded-md mt-20 mb-24 md:mb-52 w-[90%]">
-      <form @submit.prevent="handleSubmit" class="flex flex-col gap-4">
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Név:</label>
-          <input v-model="form.name" type="text" placeholder="Név" class="w-full rounded-sm border-2 border-orange-600 p-2" required />
-        </div>
+    <div class="bg-slate-950 md:w-full max-w-2xl p-10 mx-auto rounded-md mt-5 md:mt-10 mb-24 md:mb-52 w-[90%]">
+      <form @submit.prevent="handleSubmit">
+        <div class="flex flex-col lg:flex-row lg:justify-center gap-4">
+          <!-- Step 1 -->
+          <div class="flex flex-col gap-4 w-full">
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Név:</label>
+              <input v-model="form.name" type="text" placeholder="Név" class="w-full rounded-sm border-2 border-orange-600 p-2" required />
+            </div>
 
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">E-mail cím:</label>
-          <input v-model="form.email" type="email" placeholder="Email" class="w-full rounded-sm border-2 border-orange-600 p-2" required />
-        </div>
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">E-mail cím:</label>
+              <input v-model="form.email" type="email" placeholder="Email" class="w-full rounded-sm border-2 border-orange-600 p-2" required />
+            </div>
 
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Telefonszám:</label>
-          <input v-model="form.phone" type="text" placeholder="Telefonszám" class="w-full rounded-sm border-2 border-orange-600 p-2" />
-        </div>
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Telefonszám:</label>
+              <input v-model="form.phone" type="text" placeholder="Telefonszám" class="w-full rounded-sm border-2 border-orange-600 p-2" />
+            </div>
 
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Temető (város):</label>
-          <input v-model="form.cemetery" type="text" placeholder="Temető (város)" class="w-full rounded-sm border-2 border-orange-600 p-2" />
-        </div>
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Temető (város):</label>
+              <input v-model="form.cemetery" type="text" placeholder="Temető (város)" class="w-full rounded-sm border-2 border-orange-600 p-2" />
+            </div>
 
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Síremlék alapterülete:</label>
-          <input v-model="form.area" type="text" placeholder="Alapterület" class="w-full rounded-sm border-2 border-orange-600 p-2" />
-        </div>
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Síremlék alapterülete:</label>
+              <input v-model="form.area" type="text" placeholder="Alapterület" class="w-full rounded-sm border-2 border-orange-600 p-2" />
+            </div>
 
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Anyag:</label>
-          <input v-model="form.material" type="text" placeholder="Anyag" class="w-full rounded-sm border-2 border-orange-600 p-2" />
-        </div>
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Anyag:</label>
+              <input v-model="form.material" type="text" placeholder="Anyag" class="w-full rounded-sm border-2 border-orange-600 p-2" />
+            </div>
 
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Alsórész (kódszám):</label>
-          <input v-model="form.base_code" type="text" placeholder="Kódszám" class="w-full rounded-sm border-2 border-orange-600 p-2"/>
-        </div>
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Alsórész (kódszám):</label>
+              <input v-model="form.base_code" type="text" placeholder="Kódszám" class="w-full rounded-sm border-2 border-orange-600 p-2"/>
+            </div>
+          </div>
+          <!-- Step 2 -->
+          <div class="flex flex-col gap-4 w-full">
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Emlék (fejrész, kódszám):</label>
+              <input v-model="form.head_code" type="text" placeholder="Kódszám" class="w-full rounded-sm border-2 border-orange-600 p-2" />
+            </div>
 
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Emlék (fejrész, kódszám):</label>
-          <input v-model="form.head_code" type="text" placeholder="Kódszám" class="w-full rounded-sm border-2 border-orange-600 p-2" />
-        </div>
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Kiegészítők:</label>
+              <input v-model="form.extras" type="text" placeholder="Pl.: kereszt, váza, mécses..." class="w-full rounded-sm border-2 border-orange-600 p-2" />
+            </div>
 
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Kiegészítők:</label>
-          <input v-model="form.extras" type="text" placeholder="Pl.: kereszt, váza, mécses..." class="w-full rounded-sm border-2 border-orange-600 p-2" />
-        </div>
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Felirat típusa:</label>
+              <input v-model="form.inscription_type" type="text" placeholder="Felirat típusa" class="w-full rounded-sm border-2 border-orange-600 p-2" />
+            </div>
 
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Felirat típusa:</label>
-          <input v-model="form.inscription_type" type="text" placeholder="Felirat típusa" class="w-full rounded-sm border-2 border-orange-600 p-2" />
-        </div>
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Sírfelirat:</label>
+              <textarea v-model="form.inscription" placeholder="Sírfelirat" class="w-full rounded-sm border-2 border-orange-600 p-2" rows="3"></textarea>
+            </div>
 
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Sírfelirat:</label>
-          <textarea v-model="form.inscription" placeholder="Sírfelirat" class="w-full rounded-sm border-2 border-orange-600 p-2" rows="3"></textarea>
+            <div class="flex flex-col">
+              <label class="mb-2 font-semibold text-white">Üzenet:</label>
+              <textarea v-model="form.message" placeholder="Üzenet" class="w-full rounded-sm border-2 border-orange-600 p-2" rows="5"></textarea>
+            </div>
+          </div>
         </div>
-
-        <div class="flex flex-col">
-          <label class="mb-2 font-semibold text-white">Üzenet:</label>
-          <textarea v-model="form.message" placeholder="Üzenet" class="w-full rounded-sm border-2 border-orange-600 p-2" rows="5"></textarea>
-        </div>
-        
         <!-- Button -->
         <button
           type="submit"
           :disabled="isLoading"
-          class="bg-orange-600 text-white font-semibold rounded-sm p-2 mt-4 hover:bg-orange-700 disabled:opacity-50 disabled:cursor-not-allowed"
+          class="bg-orange-600 text-white font-semibold rounded-sm p-2 mt-4 hover:bg-orange-700 disabled:opacity-50 disabled:cursor-not-allowed w-full"
         >
           <span v-if="isLoading">Küldés...</span>
           <span v-else>Ajánlatkérés küldése</span>
