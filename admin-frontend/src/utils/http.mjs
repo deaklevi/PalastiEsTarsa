59727e447ad7df1a12c8ce82d10f1f6f165a72c3
deaklevi <deakleventevok@gmail.com>
2026-02-18 13:52:29 +0100

    asd

--- admin-frontend/src/utils/http.mjs
+++ admin-frontend/src/utils/http.mjs
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-    baseURL: "http://backend.vm1.test/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
     headers:{
         "Accept": "application/json",
         "Content-Type": "application/json" 
